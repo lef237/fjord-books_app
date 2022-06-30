68099100f29c679d8d2621d8ef3adb746549ee54
lef237 <lef237k5@gmail.com>
2022-07-01 04:47:56 +0900

    ymlにコントローラー用の表示を追加した。それに伴ってbooks_controller.rb内を書き換えた。メソッドを使用した。更にURLの末尾に「?locale=en」をつけると英語化されるにして、その設定がページが移動しても引き継ぐようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # URLの末尾に「?locale=en」をつけると英語化されるようになった。（ページ間のロケールの引き継ぎは未実装、ページを移動するとデフォルトの日本語に戻る）
+  # URLの末尾に「?locale=en」をつけると英語化されるようになった。
   around_action :switch_locale
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
