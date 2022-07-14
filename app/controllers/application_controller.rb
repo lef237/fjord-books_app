dc2f38324a71ca91e2186cd1c7c3fb1e7bb3d3dc
lef237 <lef237k5@gmail.com>
2022-07-14 12:47:10 +0900

    application_controller.rbに書き込んで、ログイン／ログアウト後のリダイレクトを指定した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  private
+  def after_sign_in_path_for(resource)
+    users_show_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    books_path # ログアウト後に遷移するpathを設定
+  end
+
 end
