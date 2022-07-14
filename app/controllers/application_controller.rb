4d2fb6d8cce5dadecb2d457e53508b45036d0980
lef237 <lef237k5@gmail.com>
2022-07-14 16:07:14 +0900

    ログインしていないときに内容が見れないようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!
+
   private
   def after_sign_in_path_for(resource)
     users_path # ログイン後に遷移するpathを設定
