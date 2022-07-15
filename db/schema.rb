1e05447262c8803ea39507acf9a21605b2af461a
lef237 <lef237k5@gmail.com>
2022-07-15 16:39:52 +0900

    カラムを追加して郵便番号や住所やプロフィールを保存できるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_021911) do
+ActiveRecord::Schema.define(version: 2022_07_15_073259) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2022_07_15_021911) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.text "postcode"
+    t.text "address"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
