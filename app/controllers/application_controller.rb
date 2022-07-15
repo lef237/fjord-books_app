4c77cf1a53184e377e897a04e819e1cc190944cd
lef237 <lef237k5@gmail.com>
2022-07-15 11:35:55 +0900

    ユーザーネームを登録できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@
 
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   private
   def after_sign_in_path_for(resource)
@@ -13,6 +16,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     books_path # ログアウト後に遷移するpathを設定
+  end
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :username, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 
 end
