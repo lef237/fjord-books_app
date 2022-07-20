17d7939d1b86eca141e2d7d4820109a4bd8c7d5c
lef237 <lef237k5@gmail.com>
2022-07-20 13:30:40 +0900

    models/user.rbにフォロー機能のメソッドを書いた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,19 @@ class User < ApplicationRecord
   has_many :followed, class_name: "FollowRelationship", foreign_key: "followed_id", dependent: :destroy # followedテーブル（followed_idを起点にしたテーブル）
   has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 end
