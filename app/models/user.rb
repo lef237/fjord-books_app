ae5c57e04402d1d5c89c3c887f967279d49cb9c2
lef237 <lef237k5@gmail.com>
2022-07-20 13:17:43 +0900

    models/user.rbに中間テーブルの記述をした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :follower, class_name: "FollowRelationship", foreign_key: "follower_id", dependent: :destroy # followerテーブル（follower_idを起点にしたテーブル）
+  has_many :followed, class_name: "FollowRelationship", foreign_key: "followed_id", dependent: :destroy # followedテーブル（followed_idを起点にしたテーブル）
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
 end
