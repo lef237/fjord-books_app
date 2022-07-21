747c0c64500eac446040dbd6eec1fa8c503dda2e
lef237 <lef237k5@gmail.com>
2022-07-21 23:53:27 +0900

    AddDetailsToUsersをコメントアウトして実験したらうまく動いた。

--- db/migrate/20220715073259_add_details_to_users.rb
+++ db/migrate/20220715073259_add_details_to_users.rb
@@ -1,7 +1,7 @@
 class AddDetailsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :postcode, :text
-    add_column :users, :address, :text
-    add_column :users, :profile, :text
+    # add_column :users, :postcode, :text
+    # add_column :users, :address, :text
+    # add_column :users, :profile, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,12 +27,12 @@ ActiveRecord::Schema.define(version: 2022_07_15_073259) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "postcode", default: "", null: false
+    t.text "address", default: "", null: false
+    t.text "profile", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.text "postcode"
-    t.text "address"
-    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
