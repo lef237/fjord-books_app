28478b2dcde7396c5015aa8a4228fb772d5e1e14
lef237 <lef237k5@gmail.com>
2022-07-21 23:57:52 +0900

    AddDetailsToUsersのマイグレーションファイルを削除して、bin/rails db:drop && bin/rails db:create && bin/rails db:migrateをおこなって、動作確認した。

--- db/migrate/20220715073259_add_details_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration[6.1]
-  def change
-    # add_column :users, :postcode, :text
-    # add_column :users, :address, :text
-    # add_column :users, :profile, :text
-  end
-end
