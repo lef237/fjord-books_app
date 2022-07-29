20ddca515b4baf616740fa8e111356549693b259
lef237 <lef237k5@gmail.com>
2022-07-29 16:10:12 +0900

    inverse_ofをmodels/user.rbに追加した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   # follower_relationshipsテーブル（follower_idを起点にしたテーブル）
-  has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
   # followed_relationshipsテーブル（followed_idを起点にしたテーブル）
-  has_many :followed_relationships, class_name: 'FollowRelationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followed_relationships, class_name: 'FollowRelationship', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
   # 自分がフォローしている人
   has_many :followings, through: :follower_relationships, source: :followed
   # 自分をフォローしている人
