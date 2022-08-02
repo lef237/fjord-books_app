aad0b440a81a4693a254236964380a5038dac605
lef237 <lef237k5@gmail.com>
2022-08-02 16:37:12 +0900

    rails g controller comments create destroyをおこなった。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
