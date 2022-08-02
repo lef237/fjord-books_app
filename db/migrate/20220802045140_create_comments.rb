2a19ef8cfc36dbf58846e58030c825ecc269bdf2
lef237 <lef237k5@gmail.com>
2022-08-02 16:41:19 +0900

    commentのmigrationファイルを修正した。rails g model Comment comment:string commentable:referencesと書くべきだった。

--- db/migrate/20220802045140_create_comments.rb
+++ db/migrate/20220802045140_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :comment
-      t.commentable :references, polymorphic: true
+      t.references :commentable, polymorphic: true
 
       t.timestamps
     end
