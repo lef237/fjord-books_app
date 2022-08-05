800ccccd91cda43ace4769147e385ea3f2419216
lef237 <lef237k5@gmail.com>
2022-08-05 11:48:15 +0900

    理解が難しかったところに解説のコメントを入れた。newを意識してreports_controller.rbとcomments_controller.rbを修正した。_comments.html.erbをちょっと修正した（ユーザーの名前が無いときにも対応できるようにしたい）。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,14 @@
 class CommentsController < ApplicationController
   before_action :set_commentable
 
-  # TODO: user_idをAddColumnする
-
   def create
-    # @commentはここでnewしても大丈夫？
-    p comment_params
-    p @commentable
-    # @comment = current_user.comments.new(comment_params)
-
+    # 特定のレポートもしくはブックのコメント一覧に対してnewしており、その初期値にcomment_paramsで:commentなどを渡している。特定のレポート／ユーザーに対してnewするため、commentable_typeとcommentable_idは自動的に入る。しかし、まだuser_idは入っていない。
     @comment = @commentable.comments.new(comment_params)
 
-    p @comment
-    # @comment.user_id = current_user.id
+    #user_idを@commentの中に代入する
+    # @comment = current_user.comments.new(comment_params)でも大丈夫そう？→あとで確認する
+    @comment.user_id = current_user.id
+
     if @comment.save
       redirect_to request.referer, notice: "コメントを投稿しました"
     else
@@ -33,14 +29,13 @@ class CommentsController < ApplicationController
   private
 
   def set_commentable
-    p params
-    p "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    # Routesを確認したときに、Pathの中の「:book_id」や「:report_id」と、POSTとして送られたデータがparamsとして送られる。その中にuser_idは入っていないため、このファイル内でnewする必要がある。
     @commentable = Book.find_by(params[:book_id]) if Book.find_by(params[:book_id])
     @commentable = Report.find_by(params[:report_id]) if Report.find_by(params[:report_id])
   end
 
   def comment_params
-    params.require(:comment).permit(:comment, :book_id, :report_id, :user_id)
+    params.require(:comment).permit(:comment, :book_id, :report_id)
   end
 
 end
