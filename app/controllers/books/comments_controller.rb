c9be181a2f9f5d6f5b7a6ea93fd3e4118fa480cf
lef237 <lef237k5@gmail.com>
2022-08-06 20:13:28 +0900

    bookのほうでもコメントの投稿と削除をできるようにした。

---
+++ app/controllers/books/comments_controller.rb
@@ -0,0 +1,41 @@
+class Books::CommentsController < ApplicationController
+  before_action :set_commentable, only: %i[create]
+
+  def create
+    # 特定のレポートもしくはブックのコメント一覧に対してnewしており、その初期値にcomment_paramsで:commentなどを渡している。特定のレポート／ユーザーに対してnewするため、commentable_typeとcommentable_idは自動的に入る。しかし、まだuser_idは入っていない。
+    @comment = @commentable.comments.new(comment_params)
+
+    #user_idを@commentの中に代入する
+    # @comment = current_user.comments.new(comment_params)でも大丈夫そう？→あとで確認する
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_to request.referer, notice: "コメントを投稿しました"
+    else
+      redirect_to request.referer, notice: "コメントの投稿に失敗しました"
+    end
+  end
+
+  def destroy
+    # report_idで検索する。
+    @comment = Comment.find(params[:book_id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to request.referer, notice: "コメントを削除しました"
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def set_commentable
+    # Routesを確認したときに、Pathの中の「:book_id」や「:report_id」と、POSTとして送られたデータがparamsとして送られる。その中にuser_idは入っていないため、このファイル内でnewする必要がある。
+    @commentable = Book.find(params[:book_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment, :book_id, :report_id)
+  end
+
+end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,10 @@ class BooksController < ApplicationController
 
   # GET /books/1
   # GET /books/1.json
-  def show; end
+  def show
+    @comments = @book.comments.includes(:user)
+    @comment = @book.comments.new
+  end
 
   # GET /books/new
   def new
