475f31b5568b27e4cc0bb6569106ec94551a3054
lef237 <lef237k5@gmail.com>
2022-08-29 20:46:31 +0900

    Reportクラスのインスタンスメソッドに対するモデルテストを実装した。
    前のコミットに追加の変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    my_report = Report.create(user_id: me.id, title: '題名', content: '内容')
+    her_report = Report.create(user_id: she.id, title: '題名', content: '内容')
+
+    assert my_report.editable?(me)
+    assert_not her_report.editable?(me)
+  end
+
+  test '#created_on' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    my_report = Report.create(user_id: me.id, title: '題名', content: '内容')
+    assert_equal my_report.created_on, my_report.created_at.to_date
+    assert_not_equal my_report.created_on, my_report.created_at
+  end
 end
