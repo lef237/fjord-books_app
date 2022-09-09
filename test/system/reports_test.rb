797dcd050416c82f476e5267cb5e73b0d39d221e
lef237 <lef237k5@gmail.com>
2022-09-09 18:25:13 +0900

    bobをymlファイルに追加した。余計なymlファイルやコメントアウトを削除した。

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:report_alice)
+    @report = reports(:alice_report)
 
     visit root_path
     fill_in 'Eメール', with: 'alice@example.com'
